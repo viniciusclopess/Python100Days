0edb66ef16722b80eddcd61d988d8fac2e4b29ad
vinicius.lopes <viniciusclopes2020@gmail.com>
2025-04-23 09:28:34 -0300

    d7-Hangman final

--- d7_hangman.py
+++ d7_hangman.py
@@ -1,25 +1,42 @@
 import random
+
 words_list = ['mouse', 'bamboo', 'paragraph']
 selected_word = random.choice(words_list)
-print(selected_word)
 
 blank_word = []
-i = 0
-for i in range(0, len(selected_word)):
+for i in range(len(selected_word)):
     blank_word.append('_')
     print(blank_word)
-    i+=1
 
 lives = 3
 win = False
 
 def won_the_game():
     print("Congrats! You won the game!")
+    print(f"The word was {selected_word}")
 
 def lost_the_game():
     print("Game Over. Try again next time!")
 
-while lives != 0 or win == False:
-    letter = input('Guess a letter: ').islower
-
-    continue
+while win == False:
+    if lives <= 0:
+        lost_the_game()
+        break
+    elif '_' not in blank_word:
+        won_the_game()
+        win = True
+    else:
+        letter = input('Guess a letter: ').lower()
+        i = 0
+        found = False
+        while i < len(selected_word):
+            if letter == selected_word[i] and blank_word[i] == '_':
+                blank_word[i] = letter
+                found = True
+            i += 1
+        print(blank_word)
+        if found:
+            continue
+        else:
+            lives -= 1
+            print(f"You lost a life! Lives remaining {lives}.")
